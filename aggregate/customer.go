00a15b5577ee19d87a71cb12bf7b0a180a0610c4
Arne Ziegler <arne.ziegler@edeka.de>
2025-07-02 10:36:51 +0200

    update

--- aggregate/customer.go
+++ aggregate/customer.go
@@ -1,8 +1,16 @@
 package aggregate
 
 import (
+	"errors"
 	"taverne/entity"
 	"taverne/valueobject"
+
+	"github.com/google/uuid"
+)
+
+var (
+	// ErrInvalidPerson is returned when the person is not valid in the NewCustomer factory
+	ErrInvalidPerson = errors.New("a customer has to habe an valid person")
 )
 
 type Customer struct {
@@ -13,4 +21,50 @@ type Customer struct {
 	products []*entity.Item
 	// a customer can perform many transactions
 	transactions []valueobject.Transaction
+}
+
+// NewCustomer is a factory to create a new Customer aggregate
+// It will validate that the name is not empty
+func NewCustomer(name string) (Customer, error) {
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+
+	// Create a customer object and initialize all the values to avoid nil pointer exceptions
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}, nil
+}
+
+// GetID returns the customers root entity ID
+func (c *Customer) GetID() uuid.UUID {
+	return c.person.ID
+}
+
+// SetID setzs the root ID
+func (c *Customer) SetID(id uuid.UUID) {
+	if c.person == nil {
+		c.person = &entity.Person{}
+	}
+	c.person.ID = id
+}
+
+// SetName changes the name of the customer
+func (c *Customer) SetName(name string) {
+	if c.person == nil {
+		c.person = &entity.Person{}
+	}
+	c.person.Name = name
+}
+
+// SetName changes the name of the customer
+func (c *Customer) GetName() string {
+	return c.person.Name
 }
---
+++ domain/customer/memory/memory.go
@@ -0,0 +1,43 @@
+package memory
+
+import (
+	"sync"
+	"taverne/aggregate"
+	"taverne/domain/customer"
+
+	"github.com/google/uuid"
+)
+
+// Let’s first set up the correct structure in the memory file, we want to create a struct that has methods
+// to fulfill the CustomerRepository, and let’s not forget the factory to create a new repository.
+
+// MemoryRepository fulfills the CustomerRepository interface
+type MemoryRepository struct {
+	customers map[uuid.UUID]aggregate.Customer
+	sync.Mutex
+}
+
+// new is a factory function to generate a new repository of customers
+func New() *MemoryRepository {
+	return &MemoryRepository{
+		customers: make(map[uuid.UUID]aggregate.Customer),
+	}
+}
+
+// Get finds a customer by ID
+func (mr *MemoryRepository) Get(id uuid.UUID) (aggregate.Customer, error) {
+	if customer, ok := mr.customers[id]; ok {
+		return customer, nil
+	}
+	return aggregate.Customer{}, customer.ErrCustomerNotFound
+}
+
+// Add will add a new customer to the repository
+func (mr *MemoryRepository) Add(aggregate.Customer) error {
+	return nil
+}
+
+// Update will replace an existing customer information with the new customer information
+func (mr *MemoryRepository) Update(aggregate.Customer) error {
+	return nil
+}
